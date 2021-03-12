7825964d6fd25c39f4a33ebc01f1ab5f226bfe44
Beomcheol Kwon <bc6793@gmail.com>
2021-03-12 17:46:24 +0900

    Feature : 닉네임, 캡슐네임, 위시리스트 추가 뷰 컨트롤러 생성

---
+++ TimeCapsule/NicknameViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NicknameViewController.swift
+//  
+//
+//  Created by Beomcheol Kwon on 2021/03/12.
+//
+
+import UIKit
+
+class NicknameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
---
+++ TimeCapsule/TimeCapsule/Wish/Nickname/NicknameViewController.swift
@@ -0,0 +1,54 @@
+//
+//  NicknameViewController.swift
+//  TimeCapsule
+//
+//  Created by Beomcheol Kwon on 2021/03/12.
+//
+
+import UIKit
+
+class NicknameViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var exitButton: UIButton!
+    
+    @IBOutlet weak var nameTextView: UITextView!
+    @IBOutlet weak var completionButton: UIButton!
+    @IBOutlet weak var textCountLabel: UILabel!
+    
+    @IBOutlet weak var containerView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    func setupUI() {
+        containerView.layer.cornerRadius = 30
+        containerView.borderWidth = 5
+        containerView.borderColor = .black
+        
+        completionButton.layer.cornerRadius = 8
+        completionButton.backgroundColor = UIColor.mainBlack
+        completionButton.setTitle("완료", for: .normal)
+        completionButton.setTitleColor(.white, for: .normal)
+        completionButton.setTitleColor(.white, for: .selected)
+        nameTextView.layer.cornerRadius = 9
+        nameTextView.backgroundColor = UIColor.mainGrey
+    }
+
+
+}
+
+extension NicknameViewController: UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let currentText = textView.text ?? ""
+        guard let stringRange = Range(range, in: currentText) else { return false }
+        let updatedText = currentText.replacingCharacters(in: stringRange, with: text)
+        
+        textCountLabel.text = "(\(min(updatedText.count,10))/10)"
+        return updatedText.count <= 10
+    }
+}

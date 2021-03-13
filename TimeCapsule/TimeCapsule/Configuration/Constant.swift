d5ac24083092ba1d9f0c82b41b353ea243bb249a
Beomcheol Kwon <bc6793@gmail.com>
2021-03-13 16:04:24 +0900

    Fix : 사진

--- TimeCapsule/TimeCapsule/Configuration/Constant.swift
+++ TimeCapsule/TimeCapsule/Configuration/Constant.swift
@@ -5,11 +5,12 @@
 //  Created by Zero Yoon on 2020/10/08.
 //
 
-
+import Foundation
 
 struct Constant {
     static let BASE_URL = "URL 주소를 입력해주세요"
     static let KOBIS_BASE_URL = "http://www.kobis.or.kr/kobisopenapi/webservice/rest"
-    static let testToken = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsImlhdCI6MTYxNTU1MTI1MX0.azhpQs7mOZhUBY46A9XOz_xzD18nfX59wqacFcmuqWM"
-}
+//    static let testToken = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsImlhdCI6MTYxNTU1MTI1MX0.azhpQs7mOZhUBY46A9XOz_xzD18nfX59wqacFcmuqWM"
+    static let testToken =  UserDefaults.standard.string(forKey: "loginJWTToken")!
 
+}
